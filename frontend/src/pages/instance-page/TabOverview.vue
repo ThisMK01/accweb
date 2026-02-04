a1ff2c5430d73d7a342d94abd9b3d1dcb4a6c7c7
Pedro Faria <eu@eusouopedro.com>
2026-02-04 00:37:20 +0000

    Enhance instance page with live data refresh and add GridTable and HistoriesCard components

--- frontend/src/pages/instance-page/TabOverview.vue
+++ frontend/src/pages/instance-page/TabOverview.vue
@@ -3,6 +3,8 @@ import type { LiveServerInstancePayload, InstancePayload } from '@/lib/accweb/ty
 import { liveInstance } from '@/lib/accweb/client'
 import { computed, onMounted, onUnmounted, ref, watch } from 'vue'
 import moment from 'moment'
+import HistoriesCard from './overview/HistoriesCard.vue'
+import GridTable from './overview/GridTable.vue'
 
 const props = defineProps<{ instance: InstancePayload }>()
 
@@ -10,13 +12,18 @@ let intervalId: NodeJS.Timeout
 
 const live = ref<LiveServerInstancePayload | null>(null)
 
-function loadLiveInstance(id: string) {
-  liveInstance(id)
+function refreshLiveData(id: string) {
+  return liveInstance(id)
     .then((resp) => {
       live.value = resp
-      intervalId = setTimeout(() => loadLiveInstance(id), 2000)
     })
     .catch((err) => console.error('Error fetching live instance:', err))
+}
+
+function loadLiveInstance(id: string) {
+  refreshLiveData(id).then(() => {
+    intervalId = setTimeout(() => loadLiveInstance(id), 5000)
+  })
 }
 
 const liveTs = computed(() => {
@@ -25,17 +32,17 @@ const liveTs = computed(() => {
 
 const onInit = () => {
   clearInterval(intervalId)
-  if (!props.instance.is_running) return
-
-  loadLiveInstance(props.instance.id)
+  if (props.instance.is_running) {
+    loadLiveInstance(props.instance.id)
+  } else {
+    clearInterval(intervalId)
+    refreshLiveData(props.instance.id)
+  }
 }
 
 watch(
   () => props.instance,
-  () => {
-    live.value = null
-    onInit()
-  },
+  () => onInit(),
 )
 
 onMounted(() => {
@@ -48,24 +55,37 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div v-if="live" class="flex flex-row gap-4">
+  <div class="flex flex-col gap-4">
     <div class="flex flex-auto flex-row gap-4 border rounded-lg p-3 text-sm">
-      <div class="flex-auto"><strong>Status:</strong> {{ live?.serverState }}</div>
+      <div class="flex-1"><strong>Status:</strong> {{ live?.serverState }}</div>
 
-      <div class="flex-auto"><strong>Track:</strong> {{ live?.track }}</div>
+      <div class="flex-1"><strong>Track:</strong> {{ live?.track }}</div>
 
-      <div class="flex-auto">
+      <div class="flex-2">
         <strong>Phase:</strong> {{ live?.sessionType }} ({{ live?.sessionPhase }}) [{{
           live?.sessionRemaining
         }}
         min]
       </div>
 
-      <div class="flex-auto"><strong>Drivers:</strong> {{ live?.nrClients }}</div>
+      <div class="flex-1"><strong>Drivers:</strong> {{ live?.nrClients }}</div>
 
-      <div class="flex-auto"><strong>Last Update:</strong> {{ liveTs }}</div>
+      <div class="flex-2"><strong>Last Update:</strong> {{ liveTs }}</div>
+    </div>
+
+    <div class="flex-none">
+      <div class="flex flex-row gap2">
+        <div class="flex-3">
+          <div v-if="live" class="flex flex-col gap-1">
+            <GridTable :cars="live.live!.cars" />
+          </div>
+          <div v-else>Not running...</div>
+        </div>
+
+        <div class="flex-1">
+          <HistoriesCard :live="live" v-if="live" />
+        </div>
+      </div>
     </div>
   </div>
-
-  <div v-else>Not running...</div>
 </template>
